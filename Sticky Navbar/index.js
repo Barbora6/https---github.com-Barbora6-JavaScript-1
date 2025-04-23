66c13a3361f5afae86c29097b7dc62135d0981d6
Barbora6 <smetanovabarbora@email.cz>
2025-04-23 14:31:25 +0200

    kod js

---
+++ Sticky Navbar/index.js
@@ -0,0 +1,22 @@
+// projekt 24, https://www.youtube.com/watch?v=NfvtQ2p5QG4&t=37539s
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY >
+    bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50
+  ) {
+    navbarEl.classList.add("active");
+  } else {
+    navbarEl.classList.remove("active");
+  }
+});
+
+// window.scrollY → kolik pixelů už uživatel odskroloval odshora
+
+// bottomContainerEl.offsetTop → kolik pixelů je „bottom-container“ odshora stránky
+
+// navbarEl.offsetHeight → výška navigace
+
+// - 50 → malá mezera navíc (např. na efekt)
