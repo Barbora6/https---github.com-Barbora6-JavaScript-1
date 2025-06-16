a1f84bcf88b06d28f79493d17ff41cf7862b3f9e
Barbora Smetanová <smetanovabarbora@email.cz>
2025-06-16 23:14:16 +0200

    procvičování forEach

--- Exercise/Index.js
+++ Exercise/Index.js
@@ -39,3 +39,64 @@ for (let i = 1; i <= 10; i++) {
   table.appendChild(tableRow);
   //   přidání celého řádku do tabulky
 }
+
+// _____________________________________
+
+// cyklus FOREACH
+// úkol: vypiš čísla pole do stránky
+const cyklus = document.querySelector(".cyklus-forEach");
+const cisla = [1, 2, 3, 4, 5];
+
+cisla.forEach(function (cislo) {
+  const vypisCyklus = document.createElement("p");
+  vypisCyklus.textContent = cislo;
+  cyklus.appendChild(vypisCyklus);
+});
+
+// _________________________________________
+
+// úkol 2: vypiš jména lidí do stránky s pozdravem
+
+const names = ["Petr", "Barbora", "Filip", "Matyáš", "Lukáš"];
+
+names.forEach(function (name) {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = `Ahoj ${name}, jak se dneska máš?`;
+  cyklus.appendChild(paragraph);
+});
+
+// úkol 3: sečtení všech čísel v poli
+
+const ulList = document.getElementById("number-list");
+const numbers = [12, 13, 14, 15, 16, 17, 18];
+let soucet = 0;
+
+numbers.forEach(function (number) {
+  const numberList = document.createElement("li");
+  soucet += number;
+  numberList.textContent = soucet;
+  ulList.appendChild(numberList);
+});
+
+// ___________________________________________________
+// úkol 4: přístup k indexu prvku
+const fruitList = document.getElementById("fruit-list");
+const fruits = ["jablko", "hruška", "banán", "pomeranč", "citron"];
+
+fruits.forEach(function (fruit, index) {
+  const fruitParagraph = document.createElement("p");
+  fruitParagraph.textContent = `Ovoce číslo ${index} je ${fruit}.`;
+  fruitList.appendChild(fruitParagraph);
+});
+
+// ______________________________________________________
+
+// úkol 5: převod textu na velká písmena
+const poleJmen = document.getElementById("pole-jmen");
+const jmena = ["barbora", "lenka", "anna", "josefína"];
+
+jmena.forEach(function (jmeno) {
+  const pridejJmeno = document.createElement("p");
+  pridejJmeno.textContent = `Jméno ${jmeno} jsme převedla na velká písmena: ${jmeno.toUpperCase()}`;
+  poleJmen.appendChild(pridejJmeno);
+});
